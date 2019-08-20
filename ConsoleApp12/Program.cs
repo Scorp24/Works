d42977e207e75486d36b71e46a5016bf667bb8fd
Медеуов Сана <@lcoact.itstep>
2019-08-20 18:55:21 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 1
+            string name = "Sana";
+            string surname = "Medeuov";
+
+            Console.WriteLine("Имя: {0}, Фамилия: {1}", name, surname);
+
+            Console.ReadLine();*/
+            /*
+             * 2
+            int a = 4;
+            int b = 6;
+            int c = 8;
+
+            Console.WriteLine(a + b + c);
+            Console.ReadLine();*/
+
+        }
+    }
+}
